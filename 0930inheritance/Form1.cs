03c67a1b31df8f9e6b753e9a5d8b05ef56d4ddde
= <toth.levente.21i@egressy.info>
2024-09-30 14:13:27 +0200

    0930 eddig fasza

--- 0930inheritance/Form1.cs
+++ 0930inheritance/Form1.cs
@@ -12,9 +12,12 @@ namespace _0930inheritance
 {
     public partial class Form1 : Form
     {
+        databaseHandler db;
         public Form1()
         {
             InitializeComponent();
+            db = new databaseHandler();
+            db.readAll();
         }
     }
 }
---
+++ 0930inheritance/databaseHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace _0930inheritance
+{
+    public class databaseHandler
+    {
+
+        MySqlConnection conncection;
+        Car cars = new Car();
+
+        public databaseHandler()
+        {
+            string username = "root";
+            string password = "";
+            string database = "trabant";
+            string host = "localhost";
+            string connectionString = $"host={host};username={username};password={password};database={database}";
+            conncection = new MySqlConnection(connectionString);
+            
+        }
+
+        public void readAll()
+        {
+            try
+            {
+                conncection.Open();
+                string query = $"SELECT * FROM autok";
+
+                MySqlCommand command = new MySqlCommand(query,conncection);
+                MySqlDataReader read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    Car oneCar = new Car();
+                    oneCar.ID = read.GetInt32(read.GetOrdinal("id"));
+                    oneCar.make = read.GetString("make");
+                    oneCar.model = read.GetString("model");
+                    oneCar.color = read.GetString("color");
+                    oneCar.year = read.GetInt32(read.GetOrdinal("year"));
+                    oneCar.hp = read.GetInt32(read.GetOrdinal("power"));
+                    Car.cars.Add(oneCar);
+                }
+                read.Close();
+                command.Dispose();
+                conncection.Close();
+                MessageBox.Show("fut");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);   
+            }
+        }
+    }
+}
